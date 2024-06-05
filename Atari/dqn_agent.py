a88c344155cb00f82a8d832ab66f40dad9ed3ecb
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2024-06-05 18:59:04 -0300

    Changed order

--- Atari/dqn_agent.py
+++ Atari/dqn_agent.py
@@ -8,6 +8,7 @@ import numpy as np
 from abstract_agent import Agent
 import random
 from dqn_model import DQN_Model
+import copy
 
 class DQNAgent(Agent):
     def __init__(self, env, model, obs_processing_func, memory_buffer_size, batch_size,
@@ -20,17 +21,17 @@ class DQNAgent(Agent):
         self.second_model_update = second_model_update
         self.target_policy = None
         if self.second_model_update is not None:
-            self.target_policy = DQN_Model(env.observation_space.shape[0], env.action_space.n).to(self.device)
-            self.target_policy.load_state_dict(self.policy.state_dict())
+            self.target_policy = copy.deepcopy(self.policy)
             
     def select_action(self, state, current_steps=0, train=True):
       self.epsilon = self.compute_epsilon(current_steps)
       if train and random.random() < self.epsilon:
             return torch.tensor([[self.env.action_space.sample()]], device=self.device, dtype=torch.long)
       else:
-            if self.target_policy is not None:
-                return self.target_policy(state).max(1).indices.view(1, 1)
-            return self.policy(state).max(1).indices.view(1, 1)
+            with torch.no_grad():
+              if self.target_policy is not None:
+                  return self.target_policy(state).max(1).indices.view(1, 1)
+              return self.policy(state).max(1).indices.view(1, 1)
       
     def get_optimizer(self):
         return torch.optim.Adam(self.policy.parameters(), lr=self.learning_rate)
@@ -38,29 +39,30 @@ class DQNAgent(Agent):
     def update_weights(self, total_steps):
       optimizer = self.get_optimizer()
       if len(self.memory) > self.batch_size:
-            # Resetear gradientes
-            optimizer.zero_grad()
-
             # Obtener un minibatch de la memoria. Resultando en tensores de estados, acciones, recompensas, flags de terminacion y siguentes estados.
             transitions = self.memory.sample(self.batch_size)
             batch = Transition(*zip(*transitions))
 
             # Enviar los tensores al dispositivo correspondiente.
-            states = torch.cat(batch.state).to(self.device)
-            actions = torch.cat(batch.action).to(self.device)
-            rewards = torch.cat(batch.reward).to(self.device)
-            dones = torch.cat(batch.done).to(self.device)
-            next_states = torch.cat(batch.next_state).to(self.device)
-            
-            target_policy = self.policy if self.target_policy is None else self.target_policy
-            next_values = (self.gamma * target_policy(next_states).detach().max(1).values * (1 - dones)) + rewards
+            states = torch.cat(batch.state)
+            actions = torch.cat(batch.action)
+            rewards = torch.cat(batch.reward)
+            dones = torch.cat(batch.done)
+            next_states = torch.cat(batch.next_state)
 
             q_values = self.policy(states).gather(1, actions)
+            
+            next_states_values = self.policy(next_states).max(1).values.detach() if self.target_policy is None \
+                                      else self.target_policy(next_states).max(1).values
+            next_values = (self.gamma * next_states_values * (1 - dones)) + rewards
 
             # Compute el costo y actualice los pesos.
-            loss = F.smooth_l1_loss(q_values, next_values.unsqueeze(1))
+            criterion = nn.SmoothL1Loss()
+            loss = criterion(q_values, next_values.unsqueeze(1))
 
+            optimizer.zero_grad()
             loss.backward()
+            
             nn.utils.clip_grad_value_(self.policy.parameters(), 100)
             optimizer.step()
             # En Pytorch la funcion de costo se llaman con (predicciones, objetivos) en ese orden.
