Fixed mean calculation

Atari/abstract_agent.py
@@ -90,7 +90,7 @@ class Agent(ABC):
 
         rewards.append(current_episode_reward)
         mean_reward = np.mean(rewards[-self.episode_block:])
-        mean_values = np.mean(self.get_values(states))
+        mean_values = torch.mean(self.get_values(states)).item()
 
         # Report on the traning rewards every EPISODE BLOCK episodes
         if ep % self.episode_block == 0: